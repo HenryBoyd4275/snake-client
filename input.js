9c7da859f07f0f2863f059a892355dbb625bade6
Henry Boyd <AmorphousCube@gmail.com>
2020-01-15 18:05:02 +0000

    added basic snake movement

--- input.js
+++ input.js
@@ -1,7 +1,18 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = function(input){
   if (input === '\u0003') {
-    process.stdout.write('EXIT/n');
+    process.stdout.write('EXIT\n');
     process.exit();
+  } else if (input === "w") {
+    connection.write('Move: up');
+  } else if (input === 'a') {
+    connection.write('Move: left');
+  } else if (input === 's') {
+    connection.write('Move: down');
+  } else if (input === 'd') {
+    connection.write('Move: right');
   }
 }
 
@@ -9,7 +20,8 @@ const handleUserInput = function(input){
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
